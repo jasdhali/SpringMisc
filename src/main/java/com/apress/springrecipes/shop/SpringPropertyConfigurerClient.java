f54729f1d1ca324bb518b8acbd6423f19a45e72b
jaspal dhaliwal <jaspal dhaliwal>
2016-12-06 14:22:27 -0600

    Changes to correct propertry file example

--- src/main/java/com/apress/springrecipes/shop/SpringPropertyConfigurerClient.java
+++ src/main/java/com/apress/springrecipes/shop/SpringPropertyConfigurerClient.java
@@ -1,8 +1,14 @@
 package com.apress.springrecipes.shop;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class SpringPropertyConfigurerClient {
 
@@ -14,6 +20,20 @@ public class SpringPropertyConfigurerClient {
 		System.out.println( " propertySources ==> " + propertySources.toString() );
 		Battery battery = (Battery) context.getBean("aaa");
 		System.out.println("Battery is ====> " + battery.toString() );
+		try {
+			loadRawResource();
+		} catch (IOException e) {
+			System.out.println( e.getMessage() );
+		}
+	}
+	
+	public static void loadRawResource() throws IOException{
+		Resource resource = 
+				new ClassPathResource("discounts.properties");
+		Properties props =
+				PropertiesLoaderUtils.loadProperties(resource);
+		System.out.println("And don't forget our discounts.");
+		System.out.println(props);
 	}
 	
 }
